52a9ef8fc3d14b9b4c05ca0969cd86119e9dfa7c
minwpark <clevereddie12@gmail.com>
2023-03-31 12:22:20 +0900

    week4

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,38 @@
+import 'package:eos_chat/chatting/chat/chat_bubble.dart';
 import 'package:flutter/material.dart';
 import 'package:eos_chat/screens/main_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/services.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp
     (const MyApp());
 }
 class MyApp extends StatelessWidget {
+
+
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Chatting App',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: LoginSignUpScreen() // TODO : 알맞은 위젯 채우기
+
+      debugShowCheckedModeBanner: false,
+
+      title: 'Chatting App',
+      theme: ThemeData(
+
+        appBarTheme: AppBarTheme(
+          systemOverlayStyle: SystemUiOverlayStyle.light,
+        )
+      ),
+      home: ChatScreen() // TODO : 알맞은 위젯 채우기
+      //원해는 LoginSignUpScreen
     );
   }
 }
